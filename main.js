6c4bc76a1eb343679d774417cc0bbe2990e79bd2
Akul7 <yevgeny.akulin2001@gmail.com>
2020-05-18 12:49:53 +0300

    1-я домашняя работа

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+var a = 5 ;
+var b = 3 ;
+var c = 4 ;
+var s = ((a + b + c)/3);
+alert ('Средняя оценка: ' + s);
+var name = prompt ('Ваше имя ?');
+str1 = 'Привет, мир! Меня зовут ';
+alert (str1 + name);
